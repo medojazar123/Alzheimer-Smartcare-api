8ad3ffc6a0dbe8225a29fbec5240c565aeeed2d8
medojazar123 <103908651+medojazar123@users.noreply.github.com>
2025-02-17 16:44:57 +0300

    added some folders

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c86162cb2916ee19331e19f3277896ee0f7eacf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+815ae893dc29e302b15e1baf3b1f731dc802cc2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
