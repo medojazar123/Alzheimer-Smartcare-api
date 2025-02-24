ab361a392036ec278281f6df8a6f3e3a15b4cee4
medojazar123 <103908651+medojazar123@users.noreply.github.com>
2025-02-24 22:32:49 +0300

    publish added

--- api/Program.cs
+++ api/Program.cs
@@ -81,11 +81,8 @@ builder.Services.AddControllers().AddNewtonsoftJson(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
