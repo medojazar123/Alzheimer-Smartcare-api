c12df91b9ec61a8b9cf67e208bfa619389eb50ce
medojazar123 <103908651+medojazar123@users.noreply.github.com>
2025-02-24 20:58:59 +0300

    swagger development restriction removed

--- api/Program.cs
+++ api/Program.cs
@@ -81,11 +81,10 @@ builder.Services.AddControllers().AddNewtonsoftJson(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
